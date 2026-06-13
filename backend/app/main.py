1bd0155dc5f51b47c3d620d53d56385d4825e33e
sharansree <saisharan.sreejith@gmail.com>
2026-06-12 22:37:09 -0400

    fix frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://sathya.vercel.app",
+        "https://sathya-ai-guru.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
